cea676bb7fcb5f102e59b9089c1d88052ae65b7c
Wen-Jin Xie <71911495+wenjin1997@users.noreply.github.com>
2020-09-29 20:22:34 +0800

    Add files via upload

---
+++ code/ex12.py
@@ -0,0 +1,6 @@
+age = input("How old are you? ")
+#print("How old are you?", input()) #运行会先让输入，然后把输入的内容打印出来，放在input()的相应位置
+height = input("How tall are you? ")
+weight = input("How much do you weight? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
